0c832c7b3c953c2fbd7e5740e61dfa190ce00f3a
Beatriz Lisboa <btrzlisboa037@gmail.com>
2023-05-17 14:06:59 +0100

    classes/models

---
+++ models.js
@@ -0,0 +1,25 @@
+class Users {
+    username = "";
+    email = "";
+    password = "";
+    avatar = "";
+    currentLevel = 0;
+    finishedChallenges = [];
+    badges = [];
+
+    constructor(username, email, password, avatar, currentLevel, finishedChallenges, badges){
+        username = this.username;
+        email = this.email;
+        password = this.password;
+        avatar = this.avatar;
+        currentLevel = this.currentLevel;
+        finishedChallenges = this.finishedChallenges;
+        badges = this.badges
+    }
+}
+
+class Levels {
+    levelName = "";
+    image = "";
+    numberOfChallenges= 0;
+}
