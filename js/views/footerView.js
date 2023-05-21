22b7b8d02928eb6203cc8749fffc4e5bac0d3752
Carolina Costa <carolinamccosta@gmail.com>
2023-05-21 23:47:41 +0100

    start fixing responsiveness of the homepage, navbar and footer

--- js/views/footerView.js
+++ js/views/footerView.js
@@ -2,14 +2,14 @@ function footerView() {
   
   let result = `<div class="container">
   <div class="row">
-    <div class="col-lg-3 ">
+    <div class="col-lg-3 col-sm-12 col-md-6">
       <h4 class="text my-5">MNUSE</h4>
       <ul>
         <li><a href="">Rules</a></li>
         <li><a href="">About Us</a></li>
       </ul>
     </div>
-    <div class="col-lg-3">
+    <div class="col-lg-3 col-sm-12 col-md-6">
       <h4 class="text my-5">Find Help</h4>
       <ul>
         <li><a href="https://www.nimh.nih.gov/health/find-help">National Institute of Mental Health</a></li>
@@ -17,20 +17,32 @@ function footerView() {
         <li><a href="https://www.mhe-sme.org/">Mental Health Europe</a></li>
       </ul>
     </div>
-    <div class="col-lg-3">
+    <div class="col-lg-3 col-sm-12 col-md-6">
       <h4 class="text my-5">Get Updates</h4>
       <form class="text">
-        <input type="text" placeholder="enter your email" id="subscribeBtn"><br/>
-        <button role="button" class="subscribeBtn">Subscribe</button>
+        <input type="text" placeholder="enter your email" id="subscribe"><br/>
+        <button type="button" class="btn btn-primary btn-lg" data-bs-toggle="modal" data-bs-target="#subscribeModal" id="subscribeBtn">Subscribe</button>
       </form>
     </div>
-    <div class="col-lg-3 col-12">
+    <div class="modal" id="subscribeModal">
+      <div class="modal-dialog">
+        <div class="modal-content">
+
+          <div class="modal-header">
+            <h5 class="modal-title" id="staticBackdropLabel">Sucessfully subscribed</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">Thank you for subscribing! You'll start receiving emails from us!</div>           
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-sm-12 col-md-6">
       <h4 class="text my-5">Socials</h4>
       <div class="social-links">
-        <a href="#"><i class="fab fa-facebook-f"></i></a>
-        <a href="#"><i class="fab fa-instagram"></i></a>
-        <a href="#"><i class="fab fa-twitter"></i></a>
-        <a href="#"><i class="fab fa-youtube"></i></a>
+        <a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
+        <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+        <a href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
+        <a href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a>
       </div>
     </div>
   </div>
