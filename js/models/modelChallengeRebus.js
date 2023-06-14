5cf003bbb080bbd14cbbb0c4cdb443ab84cdaf71
Beatriz Lisboa <btrzlisboa037@gmail.com>
2023-06-14 15:47:23 +0100

    att images rebus att html of memory card game

--- js/models/modelChallengeRebus.js
+++ js/models/modelChallengeRebus.js
@@ -36,14 +36,14 @@ export default class Rebus {
 
 
 
-challengesRebus.push(new Rebus("../assets/imgs/classroom-rebus1.png","one in a million","Try to separate the number inside of the word, put them in the same sentence and guess it!"))
-challengesRebus.push(new Rebus("../assets/imgs/classroom-rebus2.png","think outside the box","Way of thinking that is different from convetional. Term commonly used to instigate people to be creative and innovative."))
-challengesRebus.push(new Rebus("../assets/imgs/classroom-rebus3.png","somewhere over the rainbow","Title of a very famous song related to rainbow."))
-challengesRebus.push(new Rebus("../assets/imgs/classroom-rebus4.png","happy hour","Hour of the day where co-workers get out of the office and can enjoy the company of one another in a restaurant or a bar"))
-challengesRebus.push(new Rebus("../assets/imgs/classroom-rebus5.png","earring","Object used to garnish ones ear."))
-challengesRebus.push(new Rebus("../assets/imgs/classroom-rebus6.png","hotdog","Common streetfood originally invented in the USA"))
-challengesRebus.push(new Rebus("../assets/imgs/classroom-rebus7.png","it is raining man","Title of a pop song released in 1983 by The Weather Girls."))
-challengesRebus.push(new Rebus("../assets/imgs/classroom-rebus8.png","breakfast","Meal usually taken in the morning."))
+challengesRebus.push(new Rebus("../assets/imgs/rebus1.png","one in a million","Try to separate the number inside of the word, put them in the same sentence and guess it!"))
+challengesRebus.push(new Rebus("../assets/imgs/rebus2.png","think outside the box","Way of thinking that is different from convetional. Term commonly used to instigate people to be creative and innovative."))
+challengesRebus.push(new Rebus("../assets/imgs/rebus3.png","somewhere over the rainbow","Title of a very famous song related to rainbow."))
+challengesRebus.push(new Rebus("../assets/imgs/rebus4.png","happy hour","Hour of the day where co-workers get out of the office and can enjoy the company of one another in a restaurant or a bar"))
+challengesRebus.push(new Rebus("../assets/imgs/rebus5.png","earring","Object used to garnish ones ear."))
+challengesRebus.push(new Rebus("../assets/imgs/rebus6.png","hotdog","Common streetfood originally invented in the USA"))
+challengesRebus.push(new Rebus("../assets/imgs/rebus7.png","it is raining man","Title of a pop song released in 1983 by The Weather Girls."))
+challengesRebus.push(new Rebus("../assets/imgs/rebus8.png","breakfast","Meal usually taken in the morning."))
 localStorage.setItem("challengesRebus", JSON.stringify(challengesRebus))
 
 export function getId(){
