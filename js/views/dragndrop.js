414e94b8b6478471af4cfe5847ae8860c00800ee
Beatriz Lisboa <btrzlisboa037@gmail.com>
2023-06-14 12:32:07 +0100

    level 1 buttons back, help continue done!

--- js/views/dragndrop.js
+++ js/views/dragndrop.js
@@ -1,3 +1,6 @@
+import * as User from "./../models/modelUsers.js"
+import * as Challenges from "./../models/modelChallenges.js"
+
 const draggableElements = document.querySelectorAll(".draggable")
 const droppableElements = document.querySelectorAll(".droppable")
 
@@ -37,6 +40,7 @@ function dragLeave(event) {
   event.target.classList.remove("droppable-hover")
 }
 
+let correctAnswers = 0;
 function drop(event) {
   event.preventDefault();
   event.target.classList.remove("droppable-hover");
@@ -55,5 +59,106 @@ function drop(event) {
     droppableElement.classList.add("dropped");
     draggableElement.classList.add("dragged");
     draggableElement.setAttribute("draggable", "false");
+    correctAnswers += 1;
   }
 }
+
+
+//  BUTTON BACK
+let btnBackSideInfo = document.getElementById("btnBackSideInfo");
+btnBackSideInfo.addEventListener("click", function(event){
+    event.preventDefault();
+    User.changeLevelLoad(1);
+    window.location.href = "level.html";
+})
+
+
+// BUTTON HELP
+let textsHelpBtn = document.getElementById("textsHelpBtn");
+let challenge = Challenges.challengesList.find(chall => chall.challengeID === "dragAndDrop");
+let line = `${challenge.helpCard}`;
+textsHelpBtn.innerHTML += line;
+
+
+// BUTTON CONTINUE
+let btnContinueSideInfo = document.getElementById("btnContinueSideInfo");
+let usersList = JSON.parse(localStorage.getItem("users"));
+let user = User.getUserLogged();
+let challengeList = user.finishedChallenges;
+
+btnContinueSideInfo.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  if (correctAnswers === 3) {
+    if (!checkChallengeIs(challenge.challengeID)) {
+      challengeList.push(challenge.challengeID);
+      console.log(challenge.challengeID);
+      console.log(challengeList);
+      const updatedUser = new User.Users(user.username,user.email,user.password,user.avatar,user.currentLevel,user.levelLoad,challengeList,user.badges,user.words,user.code);
+      const index = usersList.findIndex(u => u.username === user.username);
+      usersList[index] = updatedUser;
+      sessionStorage.setItem("loggedUser", JSON.stringify(updatedUser));
+      localStorage.setItem("users", JSON.stringify(usersList));
+
+      let modal = document.getElementById("challengeSucessfullyCompleted");
+      modal.classList.add("show");
+      modal.style.display = "block";
+      document.body.classList.add("modal-open");
+    } else {
+      let modal = document.getElementById("challengeAlreadyCompleted");
+      modal.classList.add("show");
+      modal.style.display = "block";
+      document.body.classList.add("modal-open");
+      console.log(challenge.challengeID);
+      console.log(challengeList);
+    }
+  } else if (checkChallengeIs(challenge.challengeID)){
+  let modal = document.getElementById("challengeAlreadyCompleted");
+  modal.classList.add("show");
+  modal.style.display = "block";
+  document.body.classList.add("modal-open");
+  console.log(challenge.challengeID);
+  console.log(challengeList);
+  }
+  else {
+  let modal = document.getElementById("challengeNotCompleted");
+  modal.classList.add("show");
+  modal.style.display = "block";
+  document.body.classList.add("modal-open");
+  console.log(challenge.challengeID);
+  console.log(challengeList);
+  }
+})
+
+// SEE IF VIVEO CHALLENGE IS IN ARRAY OF FINISHED CHALLENGES
+function checkChallengeIs(id) {
+  let user = User.getUserLogged();
+  let challengeList = user.finishedChallenges;
+  return challengeList.includes(id);
+}
+
+
+// FUNCTIONS TO THE CLOSE BUTTON OF THE CREATED MODALS ABOVE
+let btnCloseChallengeCompleted = document.getElementById("btnCloseChallengeCompleted")
+btnCloseChallengeCompleted.addEventListener("click", function(){
+    let modal = document.getElementById("challengeAlreadyCompleted");
+    modal.classList.remove("show");
+    modal.style.display = "none";
+    document.body.classList.remove("modal-open");
+})
+
+let btnCloseChallengeSucessfull = document.getElementById("btnCloseChallengeSucessfull")
+btnCloseChallengeSucessfull.addEventListener("click", function(){
+    let modal = document.getElementById("challengeSucessfullyCompleted");
+    modal.classList.remove("show");
+    modal.style.display = "none";
+    document.body.classList.remove("modal-open");
+})
+
+let btnCloseChallengeNotCompleted = document.getElementById("btnCloseChallengeNotCompleted")
+btnCloseChallengeNotCompleted.addEventListener("click", function(){
+    let modal = document.getElementById("challengeNotCompleted");
+    modal.classList.remove("show");
+    modal.style.display = "none";
+    document.body.classList.remove("modal-open");
+})
